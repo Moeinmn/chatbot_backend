5cf8ad535a85713449bcbffa4db79b566a5fa046
moein moeinnia <moein@pro.ai>
2024-01-06 14:22:01 +0330

    chors 2

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: '*', // or a specific origin like 'http://example.com'
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    allowedHeaders: 'Content-Type, Accept',
+  });
   await app.listen(11000);
 }
 bootstrap();
